b9cfe19db7cbca27ea1ba08985250692419ce41b
mnmly <i.am@mnmly.com>
2012-01-19 06:30:19 -0600

    Added font files to main

--- fabfile.py
+++ fabfile.py
@@ -2,8 +2,9 @@ from fabric.api import *
 
 @hosts('mnmly.com')
 def deploy():
+    local("git push origin feature/font")
     code_dir = '/var/www/vhosts/tagcloud.mnmly.com'
     with cd(code_dir):
         run("sudo git pull origin feature/font")
         #run('node src/r.js -o name=main out=noun_project/core/static/js/main-built.js baseUrl=noun_project/core/static/js')
-        run("forever restart -c coffee app.coffee")
+        run("NODE_ENV=production forever restart -c coffee app.coffee")
