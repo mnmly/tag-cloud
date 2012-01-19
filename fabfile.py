9657a012dd2b5078706a447a87b4a02eb0d5f450
mnmly <i.am@mnmly.com>
2012-01-19 05:13:56 -0600

    Adding basic structure for font handling

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,6 @@ from fabric.api import *
 def deploy():
     code_dir = '/var/www/vhosts/tagcloud.mnmly.com'
     with cd(code_dir):
-        run("sudo git pull origin master")
+        run("sudo git pull origin feature/font")
         #run('node src/r.js -o name=main out=noun_project/core/static/js/main-built.js baseUrl=noun_project/core/static/js')
         run("forever restart -c coffee app.coffee")
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -16,19 +16,31 @@
       var animloop, app, count;
       window.app = app = new App(window.tweetData);
       count = 0;
-      return (animloop = function() {
+      (animloop = function() {
         if ((!(app.loadingWheel != null)) || app.loadingWheel.doneLoading) return;
         requestAnimFrame(animloop);
         return app.loadingWheel.render(count++);
       })();
+      return require(['fontplus.utils', "https://ajax.googleapis.com/ajax/libs/webfont/1.0.24/webfont.js", 'http://webfont.fontplus.jp/accessor/script/fontplus.js?LyzUQoPX3yA%3D'], function(FontPlusUtils) {
+        return app.bind('onFetchDone', function(data) {
+          var fontPlusUtils, t, text, _initial;
+          fontPlusUtils = new FontPlusUtils(WebFont);
+          text = (function() {
+            var _i, _len, _results;
+            _results = [];
+            for (_i = 0, _len = data.length; _i < _len; _i++) {
+              t = data[_i];
+              _results.push(t.tag);
+            }
+            return _results;
+          })();
+          _initial = fontPlusUtils.getFontForText('RodinPro-DB', text.join(''));
+          return fontPlusUtils.bind('fontactive', function(_uid, fontFamily, fontDescription, text) {
+            if (_initial === _uid) return console.log("Yay");
+          });
+        });
+      });
     });
-    /*
-      require [ 'font-plus-manager' ], (FontPlusManager)->
-        console.log arguments
-        app.bind 'onFetchDone', (data)->
-          
-        fontPlusManger  = new FontPlusManager
-    */
   });
 
   window.requestAnimFrame = (function() {
